ca5516ba0c809a6eb9498d08d95ece8ba3e92a5a
Ramz001 <rkenjaev1@gmail.com>
2026-03-07 22:46:06 +0500

    chore: update prettier configuration to include importOrderParserPlugins

--- src/prettier.config.mjs
+++ src/prettier.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import("prettier").Config} */
 export default {
-  trailingComma: "none",
-  tabWidth: 4,
-  useTabs: true,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid",
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderCaseInsensitive: true,
-  importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^../(.*)", "^./(.*)"],
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-};
+	trailingComma: 'none',
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	arrowParens: 'avoid',
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+	importOrderParserPlugins: [
+		'classProperties',
+		'decorators-legacy',
+		'typescript'
+	],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^../(.*)', '^./(.*)'],
+	plugins: ['@trivago/prettier-plugin-sort-imports']
+}
